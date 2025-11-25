c586e22b5147623af871e84c881771c4c2908b25
yasmin <adhm-int\y.kassem>
2025-11-25 15:19:30 +0400

    initial commit in course

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Data.Common;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    // public AppDbContext(DbContextOptions options) : base(options)// {//}
+    // And then we need to tell Entity Framework about our entities.And we do that by creating DB sets.So we create a property to use the prop snippet.
+    public DbSet<AppUser> Users { get; set; } //So if I give this a name of users then this is going to represent the table name inside the database.
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System;
+
+// Now this namespace we create this for classes.
+// So each namespace is a logical representation of where this class is located.
+// It's not a physical file although typically the namespace reflects the physical file location it is
+namespace API.Entities;
+
+// So our app user is going to represent something in a users table.
+// And each instance of this entity is going to represent a row in that table.
+// And each property that this entity class has is going to represent a column in that database.
+public class AppUser
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();  
+    // because we've given our property a name of ID, then this is the property that it's going to use as the primary key of that table in the database.
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
+}
